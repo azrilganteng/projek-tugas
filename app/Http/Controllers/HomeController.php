424a8a48db4d96a91c6523162ca28aa03774a634
azrilganteng <azrilgaming738@gmail.com>
2024-07-18 21:26:59 +0700

    memodifikasi file homecontroller agar bisa di akses

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
@@ -7,11 +8,6 @@ class HomeController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard');
-    }
-
-    public function create()
-    {
-        return view('admin.product.create');
+        return view('home'); // Pastikan Anda menggunakan fungsi 'view' dengan benar
     }
 }
